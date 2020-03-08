5e06d2f88d5bd37bbb903cf855294f75d5058184
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-03-08 15:33:24 +0900

    Updates

---
+++ precache-manifest.f49d4d23702bfcb78c58f8927eda0755.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e934447b784cdba26f5e4995eec7a8d4",
+    "url": "/corona_2020/index.html"
+  },
+  {
+    "revision": "482ade4c8cfc49414b1b",
+    "url": "/corona_2020/static/js/2.559c8171.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/corona_2020/static/js/2.559c8171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "642ad8b9f74a114463b9",
+    "url": "/corona_2020/static/js/main.3eaad732.chunk.js"
+  },
+  {
+    "revision": "ab168280d7754114f598",
+    "url": "/corona_2020/static/js/runtime-main.5f840b83.js"
+  }
+]);
